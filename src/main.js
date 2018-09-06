8c436e73e05b474f9ead10bde5dd34af739663ee
liutao19910728 <2863433495@qq.com>
2018-09-07 00:28:16 +0800

    轮播图

--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/css/base.css'
+
+
+
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 
 Vue.config.productionTip = false
-
+Vue.use(MintUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
