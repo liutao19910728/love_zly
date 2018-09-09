bf95162a70a191164c72465c929220900a972ac1
liutao19910728 <2863433495@qq.com>
2018-09-09 19:27:58 +0800

    音乐无法播放问题

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-   <audio id="music" autoplay="autoplay"
+   <audio id="music" 
    
     src="http://isure.stream.qqmusic.qq.com/C4000023ZrRN2ltlkl.m4a?vkey=556E2BC56122413FFC19CAAD1DA59F5FC1B96C4354664089B0F57F8F1C71E61A6E16E52604ACF89D691F96AA07AA140389836835D8D7AC38&guid=1767028562&uin=0&fromtag=66">
     
@@ -18,8 +18,17 @@
 
 <script>
 export default {
+  mounted() {
+    this.play();
+   
+    
+  },
   name: "App",
+
   methods: {
+    play() {
+      document.getElementById("music").play();
+    },
     autoPlayAudio() {
       wx.config({
         debug: false,
